a7ec9840e892a06cea57fc4854ee8ec9a25daab7
Alex S <as202009261140@gmail.com>
2020-12-05 16:21:22 +0300

    HQL queries

--- register-office/src/main/java/edu/javacourse/register/domain/Person.java
+++ register-office/src/main/java/edu/javacourse/register/domain/Person.java
@@ -8,6 +8,13 @@ import java.util.List;
 @Table(name = "ro_person")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "sex", discriminatorType = DiscriminatorType.INTEGER)
+@NamedQueries({
+        @NamedQuery(name = "Person.findPersons",
+        query = "SELECT p FROM Person p " +
+                "LEFT JOIN FETCH p.passports ps " +
+                "LEFT JOIN FETCH p.birthCertificate bs " +
+                "WHERE p.personId = :personId")
+})
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- register-office/src/test/java/edu/javacourse/register/dao/PersonDaoTest.java
+++ register-office/src/test/java/edu/javacourse/register/dao/PersonDaoTest.java
@@ -1,8 +1,6 @@
 package edu.javacourse.register.dao;
 
 import edu.javacourse.register.domain.Person;
-import edu.javacourse.register.domain.PersonFemale;
-import edu.javacourse.register.domain.PersonMale;
 import org.junit.Test;
 
 import java.util.List;
@@ -19,13 +17,13 @@ public class PersonDaoTest {
             System.out.println("Class for sex: " + p.getClass().getName());
             System.out.println("Passports: " + p.getPassports().size());
             System.out.println("Birth: " + p.getBirthCertificate());
-            if (p instanceof PersonMale) {
-                System.out.println("Birth Cert: " + ((PersonMale)p).getBirthCertificates().size());
-                System.out.println("Marriage Cert: " + ((PersonMale)p).getMarriageCertificates().size());
-            } else {
-                System.out.println("Birth Cert: " + ((PersonFemale)p).getBirthCertificates().size());
-                System.out.println("Marriage Cert: " + ((PersonFemale)p).getMarriageCertificates().size());
-            }
+//            if (p instanceof PersonMale) {
+//                System.out.println("Birth Cert: " + ((PersonMale)p).getBirthCertificates().size());
+//                System.out.println("Marriage Cert: " + ((PersonMale)p).getMarriageCertificates().size());
+//            } else {
+//                System.out.println("Birth Cert: " + ((PersonFemale)p).getBirthCertificates().size());
+//                System.out.println("Marriage Cert: " + ((PersonFemale)p).getMarriageCertificates().size());
+//            }
         });
     }
 }
