22500cdea2e4743b862aac31df297930cb14db08
Alex S <as202009261140@gmail.com>
2020-12-05 21:59:05 +0300

    Spring. DB is wired. Transactions

--- register-office/src/main/java/edu/javacourse/register/business/MarriageManager.java
+++ register-office/src/main/java/edu/javacourse/register/business/MarriageManager.java
@@ -3,14 +3,22 @@ package edu.javacourse.register.business;
 import edu.javacourse.register.dao.MarriageDao;
 import edu.javacourse.register.dao.PersonDao;
 import edu.javacourse.register.domain.MarriageCertificate;
+import edu.javacourse.register.domain.Person;
+import edu.javacourse.register.domain.PersonMale;
 import edu.javacourse.register.view.MarriageRequest;
 import edu.javacourse.register.view.MarriageResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@Service
+import java.time.LocalDate;
+
+@Service("marriageService")
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class MarriageManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(MarriageManager.class);
     @Autowired
@@ -18,9 +26,23 @@ public class MarriageManager {
     @Autowired
     private PersonDao personDao;
 
+    @Transactional
     public MarriageResponse findMarriageCertificate(MarriageRequest request) {
         LOGGER.info("findMarriageCertificate called");
         MarriageCertificate cert = marriageDao.findMarriageCertificate(request);
+
+        personDao.findPersons();
+        personDao.addPerson(getPerson());
+
         return new MarriageResponse();
+    }
+
+    private Person getPerson() {
+        Person m = new PersonMale();
+        m.setFirstName("1");
+        m.setLastName("2");
+        m.setPatronymic("3");
+        m.setDateOfBirth(LocalDate.of(1991,3, 12));
+        return m;
     }
 }
--- register-office/src/main/java/edu/javacourse/register/dao/PersonDao.java
+++ register-office/src/main/java/edu/javacourse/register/dao/PersonDao.java
@@ -2,23 +2,30 @@ package edu.javacourse.register.dao;
 
 import edu.javacourse.register.domain.Person;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.Query;
+import javax.persistence.*;
 import java.util.List;
 
 public class PersonDao {
+    @PersistenceContext
     private EntityManager entityManager;
-
-    public PersonDao() {
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("persistence");
-        entityManager = factory.createEntityManager();
-    }
 
     public List<Person> findPersons() {
         Query query = entityManager.createNamedQuery("Person.findPersons");
         query.setParameter("personId", 1L);
         return query.getResultList();
+    }
+
+    public Long addPerson(Person person) {
+//        EntityTransaction tr = entityManager.getTransaction();
+//        tr.begin();
+//        try {
+            entityManager.persist(person); // save person
+            entityManager.flush(); // drop all temporary data
+//            tr.commit();
+//        } catch (Exception e) {
+//            tr.rollback();
+//            throw new RuntimeException(e);
+//        }
+        return person.getPersonId();
     }
 }
