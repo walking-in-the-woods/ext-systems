65a50037cc90e2174590151e88488bf980c5ca79
Alex S <as202009261140@gmail.com>
2020-12-06 17:32:27 +0300

    Spring. JPARepository

--- register-office/src/main/java/edu/javacourse/register/dao/PersonDao.java
+++ register-office/src/main/java/edu/javacourse/register/dao/PersonDao.java
@@ -11,21 +11,12 @@ public class PersonDao {
 
     public List<Person> findPersons() {
         Query query = entityManager.createNamedQuery("Person.findPersons");
-        query.setParameter("personId", 1L);
         return query.getResultList();
     }
 
     public Long addPerson(Person person) {
-//        EntityTransaction tr = entityManager.getTransaction();
-//        tr.begin();
-//        try {
             entityManager.persist(person); // save person
             entityManager.flush(); // drop all temporary data
-//            tr.commit();
-//        } catch (Exception e) {
-//            tr.rollback();
-//            throw new RuntimeException(e);
-//        }
         return person.getPersonId();
     }
 }
