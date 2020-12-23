c7850c8c400976567dc27e1621815d3d2b167682
Alex S <as202009261140@gmail.com>
2020-12-23 22:28:42 +0300

    Spring Rest Service. Sending a file

--- student/src/main/java/edu/javacourse/student/rest/StudentController.java
+++ student/src/main/java/edu/javacourse/student/rest/StudentController.java
@@ -8,7 +8,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -34,5 +37,16 @@ public class StudentController {
     public String checkParams(@PathVariable("checkId") Long checkId,
                               @RequestParam("comment") String comment) {
         return checkId + ":" + comment;
+    }
+
+    @PostMapping(path = "/photo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String uploadPhoto(@RequestParam("comment") String comment,
+                              @RequestParam("photoFile") MultipartFile photoFile) {
+        try (InputStream is = photoFile.getInputStream()) {
+            return "Comment:" + comment + ", Name:" + photoFile.getName()
+                    + ", File Name: " + photoFile.getOriginalFilename() + ", Size: " + is.available();
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
